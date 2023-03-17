e510c17d5e66e5cf9e985b8d389a46b8453f4d35
luisrui <cr13862233007@gmail.com>
2023-03-17 15:31:36 +0800

    change and modify main files

--- DenseNet/main.py
+++ DenseNet/main.py
@@ -5,6 +5,9 @@ from torch import nn
 import torch.optim as optim
 from densenet import densenet
 from train import train, test
+import matplotlib.pyplot as plt
+import numpy as np
+from torch.utils.tensorboard import SummaryWriter
 
 norm_mean = [0.485, 0.456, 0.406]
 norm_std = [0.229, 0.224, 0.225]
@@ -17,9 +20,9 @@ transforms = torch.nn.Sequential(
 
 # hyper-parameters
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-BATCH_SIZE = 5
+BATCH_SIZE = 10
 LR = 0.001
-EPOCH = 5
+EPOCH = 10
 num_print = 10
 criterion = nn.CrossEntropyLoss()
 
@@ -39,5 +42,33 @@ test_iter = torch.utils.data.DataLoader(
     testset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
 
 if __name__ == '__main__':
-    train(model, train_iter, criterion, optimizer,
-          EPOCH, device, num_print, test_iter)
+    record_loss, record_train, record_test = train(model, train_iter, criterion, optimizer,
+                                                   EPOCH, device, num_print, test_iter)
+    torch.save(model, 'D:\\AIexperiment\\DenseNet\\savedmodels\\first_10.pth')
+    input = torch.ones((64, 3, 32, 32))
+    output = model(input)
+
+    writer = SummaryWriter('D:\\AIexperiment\\DenseNet\\result')
+    writer.add_graph(model, input)
+    for i in range(len(record_loss)):
+        writer.add_scalar("Train loss", record_loss[i], i)
+    for i in range(len(record_train)):
+        writer.add_scalar("Train accuracy", record_train[i], i)
+    for i in range(len(record_loss)):
+        writer.add_scalar("Test accuracy", record_test[i], i)
+    writer.close()
+    # print('record_test : ', record_test)
+    # print('record_train : ', record_train)
+    # plt.subplot(1, 2, 1)
+    # plt.plot(np.arange(len(record_train)) + 1, record_train)
+    # plt.title = 'train accuracy'
+    # plt.ylabel = 'accuracy'
+    # plt.xlabel = 'record times'
+
+    # plt.subplot(1, 2, 2)
+    # plt.plot(np.arange(len(record_test)) + 1, record_test)
+    # plt.title = 'test accuracy'
+    # plt.ylabel = 'accuracy'
+    # plt.xlabel = 'epochs'
+
+    # plt.savefig('./result_10_epoch.jpg')
--- DenseNet/train.py
+++ DenseNet/train.py
@@ -1,11 +1,13 @@
 import time
 from torch import nn
+import torch
 
 
-def test(net, test_iter, criterion, device):
+def test(net, test_iter, device):
+    total, correct = 0, 0
     for i, (img, label) in enumerate(test_iter):
         img, label = img.to(device), label.to(device)
-        output = net(img)
+        output = net(img.to(torch.float32))
 
         total += label.size(0)
         correct += (output.argmax(dim=1) == label).sum().item()
@@ -17,6 +19,7 @@ def train(net, train_iter, criterion, optimizer, num_epochs, device, num_print, 
     net.train()
     record_train = list()
     record_test = list()
+    record_loss = list()
 
     for epoch in range(num_epochs):
         print(
@@ -26,7 +29,7 @@ def train(net, train_iter, criterion, optimizer, num_epochs, device, num_print, 
 
         for i, (img, label) in enumerate(train_iter):
             img, label = img.to(device), label.to(device)
-            output = net(img)
+            output = net(img.to(torch.float32))
             loss = criterion(output, label)
 
             optimizer.zero_grad()
@@ -42,14 +45,14 @@ def train(net, train_iter, criterion, optimizer, num_epochs, device, num_print, 
                 print("step: [{}/{}], train_loss: {:.3f} | train_acc: {:6.3f}% | lr: {:.6f}"
                       .format(i + 1, len(train_iter), train_loss / (i + 1),
                               train_acc, optimizer.state_dict()['param_groups'][0]['lr']))
-
+            record_loss.append(train_loss / (i + 1))
         # if lr_scheduler is not None:
         #     lr_scheduler.step()
 
         print("--- cost time: {:.4f}s ---".format(time.time() - start))
 
         if test_iter is not None:
-            record_test.append(test(net, test_iter, criterion, device))
+            record_test.append(test(net, test_iter, device))
         record_train.append(train_acc)
 
-    return record_train, record_test
+    return record_loss, record_train, record_test
